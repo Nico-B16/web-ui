6573308365948a5fac17d0afd038fd18d9990904
Daniel Nosek <nosekdan@fit.cvut.cz>
2025-10-22 16:24:19 +0100

    separated styles into multiple files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './HomePage'
 import SearchPage from './SearchPage'
-import './App.css'
 
 function App() {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './styles/variables.css'
+import './styles/index.css'
+import './styles/components.css'
+import './styles/home.css'
+import './styles/search.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
